5e261fa02ece3c09340c5a0942e67f89e8d92e9e
Srinivasarao <srinivasaraouesi@gmail.com>
2021-08-01 20:39:33 +0530

    ion-slider implemented

--- src/app/patient-flow/patient-home/patient-home.page.ts
+++ src/app/patient-flow/patient-home/patient-home.page.ts
@@ -7,7 +7,12 @@ import { Router } from '@angular/router';
   styleUrls: ['./patient-home.page.scss'],
 })
 export class PatientHomePage implements OnInit {
-
+  spotlightSlideOpts = { initialSlide: 0, speed: 400, loop:true, slidesPerView: 1.5, spaceBetween: 10 };
+  doctorsSlideOpts = { initialSlide: 0, speed: 400, loop:true, slidesPerView: 1.4, spaceBetween: 20 };
+  hospitalsSlideOpts = { initialSlide: 0, speed: 400, loop:true, slidesPerView: 1.2, spaceBetween: 5 };
+  questionsSlideOpts = { initialSlide: 0, speed: 400, loop:true, slidesPerView: 1.4, spaceBetween: 5 };
+  
+  
   constructor(private router:Router) { }
 
   ngOnInit() {

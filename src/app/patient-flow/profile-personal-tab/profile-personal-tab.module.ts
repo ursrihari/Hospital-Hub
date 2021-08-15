f535f8af0576b8076a0c4e43010e661b54709682
RajithaUI <68848103+RajithaUI@users.noreply.github.com>
2021-08-15 23:50:00 +0530

    Error Fixed

--- src/app/patient-flow/profile-personal-tab/profile-personal-tab.module.ts
+++ src/app/patient-flow/profile-personal-tab/profile-personal-tab.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
-import { ProfilePersonalTabPageRoutingModule } from './profile-personal-tab-routing.module';
+//import { ProfilePersonalTabPageRoutingModule } from './profile-personal-tab-routing.module';
 
 import { ProfilePersonalTabPage } from './profile-personal-tab.page';
 
@@ -12,8 +12,8 @@ import { ProfilePersonalTabPage } from './profile-personal-tab.page';
   imports: [
     CommonModule,
     FormsModule,
-    IonicModule,
-    ProfilePersonalTabPageRoutingModule
+    IonicModule
+    //ProfilePersonalTabPageRoutingModule
   ],
   declarations: [ProfilePersonalTabPage]
 })

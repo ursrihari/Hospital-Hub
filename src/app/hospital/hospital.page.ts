f56dd3f65cd279200cba8167185f19ff6f535271
Srinivasarao <srinivasaraouesi@gmail.com>
2021-08-28 11:14:37 +0530

    added tabs in hospital page

--- src/app/hospital/hospital.page.ts
+++ src/app/hospital/hospital.page.ts
@@ -11,16 +11,32 @@ export class HospitalPage implements OnInit {
 
   canGoBack: boolean = false;
   constructor(private routerOutlet: IonRouterOutlet,private router:Router) { }
-
+  tabs:any=[];
+  activeIndex;
+  segment = 0;
   ngOnInit() {
     this.canGoBack = this.routerOutlet &&
                      this.routerOutlet.canGoBack();
+                     this.tabs = ["Today","Tomorrow","25th Aug","26th Aug","27th Aug","28th Aug","29th Aug","30th Aug","31th Aug"];
+    this.activeIndex =0;
 }
 openDoctorProfilePage(){
   this.router.navigateByUrl('/doctor-view');
 }
 bookAppointmentPage(){
   this.router.navigateByUrl('/select-doctor-time-slot');
+}
+selectTab(index){
+  this.activeIndex = index;
+  console.log(this.activeIndex);
+  //this.slider.slideTo(index);
+}
+segmentChanged() {
+   document.getElementById("segment-" + this.activeIndex).scrollIntoView({
+   behavior: 'smooth',
+   block: 'center',
+   inline: 'center'
+ });
 }
 
 }
